b9b96220235ce85eafe718af08073151df021276
cyl0221 <cyl0221@naver.com>
2024-06-11 15:57:30 +0900

    init

--- src/p01_class/AnimalEx.java
+++ src/p01_class/AnimalEx.java
@@ -1,6 +1,27 @@
 package p01_class;
 
 public class AnimalEx {
+  static Animal a2 = new Animal(); // instance 변수
+  // static 을 붙여줘야 a2가 사용가능
+  //class 내에는 멤버변수 와 method 사용
+  //Quiz) Animal을 인스턴스로 만들어 사용해보시오.
+  public void done() {
   Animal a1 = new Animal();
+    a1.run();
+    a2.run();
+  // 메인 method가 불러줘야 사용가능
+    //heap 영역
+  }
 
+  public static void main(String[] args) {    //<== method
+    Animal a1 = new Animal();
+    a1.run();
+    AnimalEx animalEx = new AnimalEx();
+        animalEx.done();
+        System.out.println(animalEx.a2);// <== 멤버 변수
+    //  메인 method 실행(jvm이 실행시킨다)
+    // jvm이 실행하기 위해서 static을 붙여서 사용한다.
+    // a2.run();//static은 같은 static 끼리만 사용 : 시점이 다름
+     a2.run();
+  }
 }
---
+++ src/p02_variable/Ex01Location.java
@@ -0,0 +1,18 @@
+package p02_variable;
+
+  public class Ex01Location {
+  // 1. 변수의 선언 위치 :: 전역, 지역
+  int sum = 0; //멤버변수는 전역(global variable)
+  // int sum = 1; // 중복 선언되어서 에러 발생
+  public void sum() {
+    System.out.println("global:"+ sum);  // 전역변수가 있어도 지역변수를 먼저 사용한다.
+    int sum =1; //지역(local variable) 중괄호 안에 선언되어진것은 지역변수
+    System.out.println("local:"+ sum);
+  }
+
+  public static void main(String[] args) {
+    Ex01Location ex01Location = new Ex01Location();
+    System.out.println(ex01Location.sum);
+    ex01Location.sum();  // 위의 sum 의 method 를 가져다 쓰기 때문에 뒤에 소괄호 작성
+  }
+}
