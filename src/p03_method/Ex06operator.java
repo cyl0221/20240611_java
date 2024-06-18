dd056f2d48ab6f297f88662707dd9ed654d114cb
cyl0221 <cyl0221@naver.com>
2024-06-18 15:11:08 +0900

    init

--- src/p03_method/Ex06operator.java
+++ src/p03_method/Ex06operator.java
@@ -2,7 +2,37 @@ package p03_method;
 
 public class Ex06operator {
   public static void main(String[] args) {
-    System.out.println("사칙 연산자");
-    System.out.println("나머지 연산자");
+    System.out.println("사칙 연산자 +,-,*,/");
+    System.out.println("나머지 연산자 %");
+    System.out.println("증감 연산자 ++, --");
+    int a = 1;
+    System.out.printf("a = %d \n", a);
+    System.out.printf("a++ = %d, ++a = %d \n", a++, ++a);
+
+    a = 1;
+    System.out.printf("a++ : %d %d %d %d %d \n",
+        a++, a++, a++, a++, a++);
+
+    a = 1;
+    System.out.printf("a++ : %d %d %d %d %d \n",
+        ++a, ++a, ++a, ++a, ++a);
+
+    a = 1;
+    int s = a++ + a++ + ++a;
+    System.out.printf("s : d% \n", s);
+    System.out.println("대입 연산자 +=, -=,*=,/=, %=");
+    System.out.println("a : " +a);
+    a += 1; //a = a +1
+    a -= 1 + s; // a = a - ( 1 + s )
+    System.out.println(a);
+    a = 1;
+    a *= 25 +a; // a = a* ( 2 + a )
+    System.out.println(a);
+    //shift 연산자
+    System.out.println("shift 연산자 >>,<<");
+    a= 2;
+    System.out.println(a<<2);// 2진수로 변환 후 왼쪽 2번 옮김.
+    System.out.println("bit 연산자 >,< ");
+
   }
 }
