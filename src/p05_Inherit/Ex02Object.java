7306d978c85b3cdfa742af2db44e582cf9b44402
cyl0221 <cyl0221@naver.com>
2024-06-20 15:40:31 +0900

    init

--- src/p05_Inherit/Ex02Object.java
+++ src/p05_Inherit/Ex02Object.java
@@ -1,4 +1,13 @@
 package p05_Inherit;
 
 public class Ex02Object {
+  public static void main(String[] args) {
+  Foo foo = new Foo();
+  String str = new String("hello");
+    System.out.println(foo);
+    System.out.println(str);
+  }
+}
+class Foo {
+
 }
