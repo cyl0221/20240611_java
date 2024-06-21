7e3881f718ffa77f8c2cf38fe4c84c798d339f18
cyl0221 <cyl0221@naver.com>
2024-06-21 10:46:08 +0900

    init

--- src/p05_Inherit/Ex03extends.java
+++ src/p05_Inherit/Ex03extends.java
@@ -4,7 +4,7 @@ public class Ex03extends {
   public static void main(String[] args) {
     //Unit unit = new Unit(); // 밑에 abstract를 사용하면 사용하지 못한다 에러
     Marine m1 = new Marine();
-    System.out.println(m1.hp);
+    System.out.println(m1);
     Medic medic = new Medic();
     System.out.println(medic);
 
@@ -13,37 +13,48 @@ public class Ex03extends {
 }
 
 abstract class Unit { /// 추상적인 class
-  public Unit(String tribe, int hp) {
+  public Unit(String tribe,String name, int hp) {
     super();
     this.tribe = tribe;
+    this.name = name;
     this.hp = hp;
+
+
   }
 
   int hp;
-   String tribe;
+  String tribe;
+  String name;
   public void move(int x, int y){}
   public void stop(){}
 
   @Override
   public String toString() {
-    return String.format("Unit{hp=%d, tribe=%s}",hp,tribe);
+    return String.format("%s{tribe=%s name=%s, hp=%d }",name,tribe,name,hp);
   }
 }
-abstract class Terran extends  Unit{  // 상속할때 extends 를 사용한다
+abstract class Protoss extends  Unit{  // 상속할때 extends 를 사용한다
   // 상속할 경우에는 Member변수와 method만 상속 // 생성자는 상속이 안된다.
-  public Terran(int hp) {
+  public Protoss(String name, int hp) {
     //super <= (생략되어있음)// 내가 상속받을 클래스를 가르킴(조상의 생성자)
-    super("Terran",hp);
+    super("Protoss",name,hp);
+  }
+  // 추상클래스  스스로 인스턴스 생성불가, 상속받은 후 인스턴스 생성 가능
+}abstract class Terran extends  Unit{  // 상속할때 extends 를 사용한다
+  // 상속할 경우에는 Member변수와 method만 상속 // 생성자는 상속이 안된다.
+  public Terran(String name, int hp) {
+    //super <= (생략되어있음)// 내가 상속받을 클래스를 가르킴(조상의 생성자)
+    super("Terran",name,hp);  //ctrl + super 를 클릭하면 조상(terran의 조상 Unit)을 알 수 있음
   }
 }
 class Marine extends Terran {
   public Marine() {
-    super(60);
+    super("Marine",60);
   }
 }
 class  Medic extends Terran{
   public Medic() {
-    super(45);
+    super("Medic",45);
   }
 }
 
