542effdbbd2ffe8024aa44248c73d8a8a41261cf
cyl0221 <cyl0221@naver.com>
2024-06-26 12:03:25 +0900

    240626

--- src/p07_Collection/Ex08Map.java
+++ src/p07_Collection/Ex08Map.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class Ex08Map {
   public static void main(String[] args) {
-    Map map = new HashMap();
+    Map<String, String> map = new HashMap(); // ----(1)
     map.put("admin", "1");
     map.put("user", "1");
     if (map.containsKey("member") == false) map.put("member", "1");
@@ -14,6 +14,8 @@ public class Ex08Map {
     if (!map.containsKey("member")) map.put("member", "2");
     //if문 에서는 조건이 맞아야지 if문내에 내용이 실행된다, 그게 아니라면 없는거나 다름없다
     map.put("member", "3"); //키가 중복될때 맨앞에 들어온값에 맨뒤에 들어온값이 덮어 쓰게된다. overwrite
+    System.out.println(map);
+    map.remove("admin");
     System.out.println(map);
 
     // 1) key 와 value를 동시에 접근
@@ -24,6 +26,21 @@ public class Ex08Map {
           entry.getKey(),
           entry.getValue());
     }
-  }
+    // 2) key 로 접근
+    it = map.keySet().iterator();
+    while (it.hasNext()) {
+      String key = (String) it.next();
+      String value = map.get(key); //error 가 났을때 위에서 (1) 에 <String String>을 적어준다.
+      System.out.printf("k: %s/ v : %s \n",key,value);
+
+    }
+    // 3) value 접근
+    it = map.values().iterator();
+    while (it.hasNext()) {
+      String value = (String) it.next();
+      System.out.printf(value);
+
+    }
+   }
 }
 
---
+++ src/p07_Collection/Ex09MapApplicate.java
@@ -0,0 +1,24 @@
+package p07_Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex09MapApplicate {
+  private static Map phoneBook = new HashMap();
+  public static void main(String[] args) {
+    addPhone("친구", "김수한", "010-1111-1110");
+    addPhone("친구", "김수한", "010-1111-1111");
+    addPhone("친구", "김수한", "010-1111-1112");
+    addPhone("친구", "김수한", "010-1111-1113");
+    addPhone("마트", "010-1111-1114");
+    printBook(phoneBook);
+  }
+  private static void addPhone(String 친구, String 김수한, String s) {
+
+  }private static void addPhone(String 김수한, String s) { // overloading 상태
+
+  }
+  private static void printBook(Map phoneBook) {
+
+  }
+}
